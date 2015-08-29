a564a866c2c0113752a7f18a612500c49865a649
ddqre <ddqre@qq.com>
2015-08-29 10:51:02 +0800

    Init

---
+++ src/ChatBot/ChatBotForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ChatBot
+{
+    partial class ChatBotForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatBotForm));
+            this.txtReply = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnGetReply = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonSpeech = new System.Windows.Forms.RadioButton();
+            this.radioButtonHand = new System.Windows.Forms.RadioButton();
+            this.btnSpeech = new System.Windows.Forms.Button();
+            this.mainMenu.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtReply
+            // 
+            this.txtReply.Location = new System.Drawing.Point(12, 96);
+            this.txtReply.Multiline = true;
+            this.txtReply.Name = "txtReply";
+            this.txtReply.ReadOnly = true;
+            this.txtReply.Size = new System.Drawing.Size(274, 124);
+            this.txtReply.TabIndex = 1;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 43);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(274, 21);
+            this.txtSend.TabIndex = 0;
+            this.txtSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSend_KeyPress);
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.helpMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(424, 24);
+            this.mainMenu.TabIndex = 2;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadMenuItem,
+            this.toolStripSeparator1,
+            this.exitMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileMenuItem.Text = "File";
+            // 
+            // loadMenuItem
+            // 
+            this.loadMenuItem.Name = "loadMenuItem";
+            this.loadMenuItem.Size = new System.Drawing.Size(209, 22);
+            this.loadMenuItem.Text = "Choose KnowledgeBase...";
+            this.loadMenuItem.Click += new System.EventHandler(this.loadMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(206, 6);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(209, 22);
+            this.exitMenuItem.Text = "Quit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutMenuItem});
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpMenuItem.Text = "Help";
+            // 
+            // aboutMenuItem
+            // 
+            this.aboutMenuItem.Name = "aboutMenuItem";
+            this.aboutMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutMenuItem.Text = "About...";
+            this.aboutMenuItem.Click += new System.EventHandler(this.aboutMenuItem_Click);
+            // 
+            // btnGetReply
+            // 
+            this.btnGetReply.Location = new System.Drawing.Point(305, 43);
+            this.btnGetReply.Name = "btnGetReply";
+            this.btnGetReply.Size = new System.Drawing.Size(99, 21);
+            this.btnGetReply.TabIndex = 3;
+            this.btnGetReply.Text = "Get Reply";
+            this.btnGetReply.UseVisualStyleBackColor = true;
+            this.btnGetReply.Click += new System.EventHandler(this.btnGetReply_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.radioButtonSpeech);
+            this.panel1.Controls.Add(this.radioButtonHand);
+            this.panel1.Location = new System.Drawing.Point(305, 96);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(99, 88);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButtonSpeech
+            // 
+            this.radioButtonSpeech.AutoSize = true;
+            this.radioButtonSpeech.Location = new System.Drawing.Point(18, 18);
+            this.radioButtonSpeech.Name = "radioButtonSpeech";
+            this.radioButtonSpeech.Size = new System.Drawing.Size(59, 16);
+            this.radioButtonSpeech.TabIndex = 1;
+            this.radioButtonSpeech.Text = "Speech";
+            this.radioButtonSpeech.UseVisualStyleBackColor = true;
+            this.radioButtonSpeech.CheckedChanged += new System.EventHandler(this.radioButtonSpeech_CheckedChanged);
+            // 
+            // radioButtonHand
+            // 
+            this.radioButtonHand.AutoSize = true;
+            this.radioButtonHand.Checked = true;
+            this.radioButtonHand.Location = new System.Drawing.Point(18, 52);
+            this.radioButtonHand.Name = "radioButtonHand";
+            this.radioButtonHand.Size = new System.Drawing.Size(47, 16);
+            this.radioButtonHand.TabIndex = 0;
+            this.radioButtonHand.TabStop = true;
+            this.radioButtonHand.Text = "Text";
+            this.radioButtonHand.UseVisualStyleBackColor = true;
+            this.radioButtonHand.CheckedChanged += new System.EventHandler(this.radioButtonHand_CheckedChanged);
+            // 
+            // btnSpeech
+            // 
+            this.btnSpeech.Enabled = false;
+            this.btnSpeech.Location = new System.Drawing.Point(305, 199);
+            this.btnSpeech.Name = "btnSpeech";
+            this.btnSpeech.Size = new System.Drawing.Size(99, 21);
+            this.btnSpeech.TabIndex = 5;
+            this.btnSpeech.Text = "Start";
+            this.btnSpeech.UseVisualStyleBackColor = true;
+            this.btnSpeech.Click += new System.EventHandler(this.btnSpeech_Click);
+            // 
+            // ChatBotForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 245);
+            this.Controls.Add(this.btnSpeech);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGetReply);
+            this.Controls.Add(this.txtReply);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.mainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "ChatBotForm";
+            this.Text = "ChatBot";
+            this.Load += new System.EventHandler(this.ChatBotForm_Load);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtReply;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutMenuItem;
+        private System.Windows.Forms.Button btnGetReply;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButtonSpeech;
+        private System.Windows.Forms.RadioButton radioButtonHand;
+        private System.Windows.Forms.Button btnSpeech;
+    }
+}
+
---
+++ src/ChatBot/ChatBotForm.cs
@@ -0,0 +1,279 @@
+﻿/*
+ * Author  : Copyright (c) Calinyara
+ * E-mail  : calinyara@gmail.com
+ * 
+ * ChatBot
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Conversive.Verbot5;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace ChatBot
+{
+    public partial class ChatBotForm : Form
+    {
+        Verbot5Engine verbot;
+        State state;
+        string stCKBFileFilter = "KnowledgeBase file (*.ckb)|*.ckb";
+        string stFormName = "ChatBot";
+
+        SpeechSynthesizer speaker;      // define speech synthesis engine
+        SpeechRecognitionEngine recognizer;    // define speech recognition engine
+
+
+        public ChatBotForm()
+        {
+            InitializeComponent();
+
+            this.verbot = new Verbot5Engine();
+            this.state = new State();
+            this.speaker =  new SpeechSynthesizer();
+            this.recognizer = new SpeechRecognitionEngine();
+            this.openFileDialog1.Filter = this.stCKBFileFilter;
+            this.Text = this.stFormName;
+
+        }
+
+        private void txtSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')
+            {
+                e.Handled = true;
+                this.getReply(this.txtSend.Text.Trim());
+            }
+        }
+
+
+        private string getReply(string str)
+        {
+            string stInput = str;
+            this.txtSend.Text = "";
+            Reply reply = this.verbot.GetReply(stInput, this.state);
+            if (reply != null)
+            {
+                this.txtReply.Text = "You: " + str + "\r\n" + "julia:" + reply.Text;
+                this.parseEmbeddedOutputCommands(reply.AgentText);
+                this.runProgram(reply.Cmd);
+                return reply.Text;
+            }
+            else
+            {
+                this.txtReply.Text = "You: " + str + "\r\n" + "julia: pardon?!";
+                return "pardon?!";
+            }
+        }
+
+
+        private void btnGetReply_Click(object sender, EventArgs e)
+        {
+            this.getReply(this.txtSend.Text.Trim());
+        }
+
+
+        private void loadMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.RestoreDirectory = true;
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.verbot.AddCompiledKnowledgeBase(this.openFileDialog1.FileName);
+                this.state.CurrentKBs.Clear();
+                this.state.CurrentKBs.Add(this.openFileDialog1.FileName);
+            }
+        }
+
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void parseEmbeddedOutputCommands(string text)
+        {
+            string startCommand = "<";
+            string endCommand = ">";
+
+            int start = text.IndexOf(startCommand);
+            int end = -1;
+
+            while (start != -1)
+            {
+                end = text.IndexOf(endCommand, start);
+                if (end != -1)
+                {
+                    string command = text.Substring(start + 1, end - start - 1).Trim();
+                    if (command != "")
+                    {
+                        this.runEmbeddedOutputCommand(command);
+                    }
+                }
+                start = text.IndexOf(startCommand, start + 1);
+            }
+        }//parseEmbeddedOutputCommands(string text)
+
+        private void runEmbeddedOutputCommand(string command)
+        {
+            int spaceIndex = command.IndexOf(" ");
+
+            string function;
+            string args;
+            if (spaceIndex == -1)
+            {
+                function = command.ToLower();
+                args = "";
+            }
+            else
+            {
+                function = command.Substring(0, spaceIndex).ToLower();
+                args = command.Substring(spaceIndex + 1);
+            }
+
+            try
+            {
+                switch (function)
+                {
+                    case "quit":
+                    case "exit":
+
+                        this.Close();
+                        break;
+                    case "run":
+                        this.runProgram(args);
+                        break;
+                    default:
+                        break;
+                }//switch
+            }
+            catch { }
+        }//runOutputEmbeddedCommand(string command)
+
+        private void runProgram(string command)
+        {
+            try
+            {
+                string[] pieces = this.splitOnFirstUnquotedSpace(command);
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = pieces[0].Trim();
+                proc.StartInfo.Arguments = pieces[1];
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.UseShellExecute = true;
+                proc.Start();
+            }
+            catch { }
+        }//runProgram(string filename, string args)
+
+        public string[] splitOnFirstUnquotedSpace(string text)
+        {
+            string[] pieces = new string[2];
+            int index = -1;
+            bool isQuoted = false;
+            //find the first unquoted space
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '"')
+                    isQuoted = !isQuoted;
+                else if (text[i] == ' ' && !isQuoted)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            //break up the string
+            if (index != -1)
+            {
+                pieces[0] = text.Substring(0, index);
+                pieces[1] = text.Substring(index + 1);
+            }
+            else
+            {
+                pieces[0] = text;
+                pieces[1] = "";
+            }
+
+            return pieces;
+        }//splitOnFirstUnquotedSpace(string text)
+
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, this.stFormName + "\r\nAuthor: Calinyara\r\n" + "Version: " +
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void radioButtonSpeech_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSpeech.Enabled = true;
+        }
+
+        private void InitializeSpeechEngine()
+        {
+            recognizer.SetInputToDefaultAudioDevice();
+            Grammar G  = new DictationGrammar();
+            recognizer.LoadGrammar(G);
+            G.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(G_SpeechRecognized);
+        }
+
+
+        private void InitializeKnowledgeBase()
+        {
+            this.verbot.AddCompiledKnowledgeBase(Application.StartupPath + @"\julia.ckb");
+            this.state.CurrentKBs.Clear();
+            this.state.CurrentKBs.Add(Application.StartupPath + @"\julia.ckb");
+        }
+
+        void G_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string str = this.getReply(e.Result.Text.Trim());
+            //recognizer.RecognizeAsyncCancel(); 
+            speaker.Speak(str);
+            //recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void radioButtonHand_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSpeech.Enabled = false;
+            btnSpeech.Text = "start";
+        }
+
+        private void btnSpeech_Click(object sender, EventArgs e)
+        {
+            if (radioButtonSpeech.Enabled==true)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                btnSpeech.Text = "Stop";
+                radioButtonSpeech.Enabled = false;
+                radioButtonHand.Enabled = false;
+            } 
+            else
+            {
+                recognizer.RecognizeAsyncCancel();
+                btnSpeech.Text = "Start";
+                radioButtonHand.Enabled = true;
+                radioButtonSpeech.Enabled = true;
+            }
+        }
+
+        private void ChatBotForm_Load(object sender, EventArgs e)
+        {
+            InitializeKnowledgeBase();
+            InitializeSpeechEngine();
+        }
+
+    }
+}
+
